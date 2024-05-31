e4aae570db521efc6b00e84f7a433964d57434b2
midnight <midnight3424@gmail.com>
2024-05-31 12:08:04 -0500

    chore: Add Flowbite CSS framework and initialize it on app mount

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <template>
