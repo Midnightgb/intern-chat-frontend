4fbea58de7da4ff426f5d374c228557f0a899cf7
midnight <midnight3424@gmail.com>
2024-05-31 12:21:08 -0500

    chore: Initialize Flowbite CSS framework on app mount and remove unused code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
-
-onMounted(() => {
-    initFlowbite();
-})
 </script>
 
 <template>
