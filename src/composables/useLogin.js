76d90f178d33f826eb039dd9f22bdf3125b39e34
midnight <midnight3424@gmail.com>
2024-07-12 09:07:28 -0500

    refactor: Update API endpoints and routes configuration

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -1,7 +1,8 @@
+//src/composables/useLogin.js
 import { reactive } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { loginValidators } from '@/utils/validators';
-import apiClient from '@/services/api'
+import { login } from '@/services/api'
 import { useRouter } from 'vue-router'
 
 export default function useLogin() {
@@ -18,7 +19,7 @@ export default function useLogin() {
     const isFormCorrect = await v$.value.$validate()
     if (isFormCorrect) {
       try {
-        const response = await apiClient.post('auth/login', {
+        const response = await login({
           network_user: state.network_user,
           password: state.password
         })
