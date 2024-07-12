d1a6a01ee572d2c7d940c509ae9c9312fd8d8448
Ramirez-05 <ramirezcardona2204@gmail.com>
2024-07-12 11:15:27 -0500

    refactor: Add getChannels API function and update login logic

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -2,20 +2,21 @@
 import { reactive } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { loginValidators } from '@/utils/validators';
-import { login } from '@/services/api'
+import { login, getChannels } from '@/services/api'
 import { useRouter } from 'vue-router'
-import ToastNotification from '@/components/layout/ToastNotification.vue'; 
+import ToastNotification from '@/components/layout/ToastNotification.vue';
+
 
 export default function useLogin() {
   const router = useRouter()
-
+  
   const state = reactive({
     network_user: '',
     password: ''
   })
-
+  
   const v$ = useVuelidate(loginValidators, state)
-
+  
   const handleSubmit = async () => {
     const isFormCorrect = await v$.value.$validate()
     if (isFormCorrect) {
@@ -24,12 +25,18 @@ export default function useLogin() {
           network_user: state.network_user,
           password: state.password
         })
-
+        
         console.log('Login successful', response.data)
-
+        
         localStorage.setItem('token', response.data.token)
+        
+        if (response.data.success === true) {
+          const channels = await getChannels()
+          console.log('Channels', channels.data)
+        }
 
         router.push('/dashboard')
+        
         ToastNotification.methods.showToast('success', 'Login successful');
 
       } catch (error) {
--- src/services/api.js
+++ src/services/api.js
@@ -2,20 +2,33 @@
 import axios from 'axios';
 import { API_ENDPOINTS } from '@/constants/apiEndpoints';
 
-const apiClient = axios.create({
+const publicApi = axios.create({
   baseURL: API_ENDPOINTS.BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
+const apiClient = axios.create({
+  baseURL: API_ENDPOINTS.BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem('token')}`
+  }
+});
+
 // Funciones de API
 export const login = (credentials) => {
-  return apiClient.post(API_ENDPOINTS.LOGIN, credentials);
+  return publicApi.post(API_ENDPOINTS.LOGIN, credentials);
 };
 
 export const logout = () => {
   return apiClient.post(API_ENDPOINTS.LOGOUT);
 };
+
+export const getChannels = () => {
+  return apiClient.get(API_ENDPOINTS.GET_CHANNELS);
+};
+
 
 export default apiClient;
