9be22d7e1d943e92ccb39007ccb2481a53f2011a
Ramirez-05 <ramirezcardona2204@gmail.com>
2024-07-16 10:38:00 -0500

    no funca

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full max-w-sm p-6 bg-orange-200 rounded-lg shadow-md">
+ <!--  <div class="w-full max-w-sm p-6 bg-orange-200 rounded-lg shadow-md">
     <h1 class="mb-6 text-2xl font-bold text-center text-gray-900">Dashboard</h1>
     <p class="text-center text-gray-700">Welcome back, {{ user.name }}!</p>
     <p class="text-center text-gray-700">You are logged in as {{ user.role }}.</p>
@@ -10,11 +10,24 @@
     >
       Logout
     </button>
+  </div> -->
+
+  <div>
+    <NavBar />
   </div>
+
+
 </template>
 
 <script setup>
-import { useAuth } from '@/composables/useAuth'
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+import NavBar from '@/components/partials/NavBar.vue'
+/* import { useAuth } from '@/composables/useAuth'
 import { useAuthHandlers } from '@/handlers'
 import { storeToRefs } from 'pinia'
 import { useAuthStore } from '@/stores/auth'
@@ -28,5 +41,5 @@ const { handleLogout: handlersLogout } = useAuthHandlers()
 const handleLogout = () => {
   authLogout()
   handlersLogout()
-}
+} */
 </script>
