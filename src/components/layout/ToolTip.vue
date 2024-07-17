772c1967d5340ea299fec079fe2686c54efe750b
Midnightgb <midnight3424@gmail.com>
2024-07-16 21:26:14 -0500

    feat: Add DropDown and ToolTip components

---
+++ src/components/layout/ToolTip.vue
@@ -0,0 +1,35 @@
+<template>
+  <fwb-tooltip>
+    <template #trigger>
+      <fwb-button color="dark">
+        {{ triggerText }}
+        <slot name="triggerIcon"></slot>
+        <Settings />
+      </fwb-button>
+    </template>
+    <template #content>
+      {{ tooltipContent }}
+    </template>
+  </fwb-tooltip>
+</template>
+
+<script setup>
+import { FwbButton, FwbTooltip } from 'flowbite-vue'
+import { Settings } from 'lucide-vue-next';
+
+defineProps({
+  triggerText: {
+    type: String,
+    required: true
+  },
+  triggerIcon: {
+    type: String,
+    required: false
+  },
+  tooltipContent: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
