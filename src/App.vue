cb4f70b07d755fcc5e816b498488773b33b3e97c
Midnightgb <midnight3424@gmail.com>
2024-07-16 19:09:59 -0500

    refactor: Update App.vue to improve layout and authentication handling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-green-100">
-    <router-view />
+  <div class="min-h-screen">
+    <template v-if="showAuthContent">
+      <div class="flex bg-gray-100 text-black">
+        <SidebarLeft />
+        <SideBarRight />
+        <MainContent>
+          <RouterView />
+        </MainContent>
+      </div>
+    </template>
+    <template v-else>
+      <div class="flex items-center justify-center bg-green-100">
+        <RouterView />
+      </div>
+    </template>
   </div>
 </template>
+
+<script setup>
+import { onMounted, computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+import { initFlowbite } from 'flowbite'
+import SidebarLeft from './components/partials/SideBarLeft.vue'
+import SideBarRight from './components/partials/SideBarRight.vue'
+import MainContent from './components/MainContent.vue'
+
+onMounted(() => {
+  initFlowbite()
+})
+
+const route = useRoute()
+const authStore = useAuthStore()
+
+const showAuthContent = computed(() => {
+  return authStore.isAuthenticated && route.name !== 'login'
+})
+</script>
