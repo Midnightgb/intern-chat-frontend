92e674c6a522475b78af60f928c9878d69a84814
midnight <midnight3424@gmail.com>
2024-08-03 11:13:24 -0500

    refactor: Update ChatContent.vue and MessageList.vue components for better user experience

---
+++ src/components/chat/ChatContent.vue
@@ -0,0 +1,47 @@
+//src/components/chat/ChatContent.vue
+<template>
+  <div class="flex-1 px-6 pt-6 overflow-hidden flex flex-col">
+    <!-- Mostrar mensaje cuando no hay selección -->
+    <div v-if="!currentId" class="flex items-center justify-center h-full">
+      <h2 class="text-2xl font-bold text-gray-500">{{ noSelectionMessage }}</h2>
+    </div>
+    <!-- Mostrar loader cuando se están cargando los datos -->
+    <div v-else-if="isLoading" class="flex items-center justify-center h-full">
+      <fwb-spinner color="gray" size="10" />
+    </div>
+    <!-- Mostrar contenido cuando los datos estén listos -->
+    <div v-else class="flex flex-col h-full">
+      <div class="flex items-center justify-between mb-2 border-b">
+        <ServerHeader :serverName="currentName" />
+      </div>
+      <div class="flex-1 bg-muted rounded-lg px-4 overflow-hidden flex flex-col">
+        <MessageList class="flex-1 overflow-y-auto" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { FwbSpinner } from 'flowbite-vue'
+import MessageList from '@/components/chat/MessageList.vue'
+import ServerHeader from '@/components/partials/ServerHeader.vue'
+
+defineProps({
+  currentId: {
+    type: Number,
+    default: null
+  },
+  currentName: {
+    type: String,
+    default: ''
+  },
+  isLoading: {
+    type: Boolean,
+    default: false
+  },
+  noSelectionMessage: {
+    type: String,
+    default: 'Por favor, selecciona una opción.'
+  }
+})
+</script>
--- src/components/layout/MainContent.vue
+++ src/components/layout/MainContent.vue
@@ -1,52 +1,51 @@
 //src/components/layout/MainContent.vue
 <template>
   <main class="flex-1 border-gray-300 rounded-tr-xl bg-background flex flex-col h-screen">
-    <!-- Contenido principal -->
-    <div class="flex-1 px-6 pt-6 overflow-hidden flex flex-col">
-      <!-- Mostrar mensaje cuando el canal no esté seleccionado -->
-      <div v-if="currentChannelId === null" class="flex items-center justify-center h-full">
-        <h2 class="text-2xl font-bold text-gray-500">Por favor, selecciona un canal.</h2>
-      </div>
-      <!-- Mostrar loader cuando el canal esté seleccionado pero los datos no se hayan cargado -->
-      <div v-else-if="isLoadingMessages" class="flex items-center justify-center h-full">
-        <fwb-spinner color="gray" size="10" />
-      </div>
-      <!-- Mostrar contenido del canal cuando los datos estén listos -->
-      <div v-else class="flex flex-col h-full">
-        <div class="flex items-center justify-between mb-2 border-b">
-          <ServerHeader :serverName="currentChannelName" />
-        </div>
-        <div class="flex-1 bg-muted rounded-lg px-4 overflow-hidden flex flex-col">
-          <MessageList class="flex-1 overflow-y-auto" />
-        </div>
-      </div>
-    </div>
-
+    <ChatContent
+      :currentId="currentId"
+      :currentName="currentName"
+      :isLoading="isLoading"
+      :noSelectionMessage="noSelectionMessage"
+    />
     <!-- Barra inferior con MessageInput -->
-    <div v-if="currentChannelId" class="border-t border-gray-300 p-4 bg-background">
-      <MessageInput />
+    <div v-if="currentId" class="border-t border-gray-300 p-4 bg-background">
+      <MessageInput @send-message="sendMessage" />
     </div>
   </main>
 </template>
 
 <script setup>
+import { computed, onMounted, onUnmounted, watch } from 'vue'
 import { storeToRefs } from 'pinia'
-import { onMounted, onUnmounted, watch } from 'vue'
-// Services
+import { useRoute } from 'vue-router'
 import { socketService } from '@/services/socketService'
-// Stores
 import { useCurrentChannelStore } from '@/stores/channels/currentChannelStore'
 import { useMessageStore } from '@/stores/messages/messageStore'
-// Components
-import { FwbSpinner } from 'flowbite-vue'
+import ChatContent from '@/components/chat/ChatContent.vue'
 import MessageInput from '@/components/chat/MessageInput.vue'
-import MessageList from '@/components/chat/MessageList.vue'
-import ServerHeader from '@/components/partials/ServerHeader.vue'
 
+const route = useRoute()
 const currentChannelStore = useCurrentChannelStore()
 const messageStore = useMessageStore()
+
 const { currentChannelId, currentChannelName } = storeToRefs(currentChannelStore)
-const { loadingMessages: isLoadingMessages } = storeToRefs(messageStore)
+const { loadingMessages, loadingConversations } = storeToRefs(messageStore)
+
+const currentId = computed(() => {
+  return route.name === 'direct' ? route.params.userId : currentChannelId.value
+})
+
+const currentName = computed(() => {
+  return route.name === 'direct' ? 'Mensaje Directo' : currentChannelName.value
+})
+
+const isLoading = computed(() => {
+  return route.name === 'direct' ? loadingConversations.value : loadingMessages.value
+})
+
+const noSelectionMessage = computed(() => {
+  return route.name === 'direct' ? 'Por favor, selecciona un usuario.' : 'Por favor, selecciona un canal.'
+})
 
 onMounted(() => {
   socketService.connect()
@@ -56,9 +55,21 @@ onUnmounted(() => {
   socketService.disconnect()
 })
 
-watch(currentChannelId, (newChannelId) => {
-  if (newChannelId) {
-    socketService.joinChannel(newChannelId)
+watch(currentId, (newId) => {
+  if (newId) {
+    if (route.name === 'direct') {
+      socketService.getDirectMessages(newId, route.params.recipientId)
+    } else {
+      socketService.joinChannel(newId)
+    }
   }
 })
+
+function sendMessage(message) {
+  if (route.name === 'direct') {
+    socketService.sendDirectMessage(currentId.value, message)
+  } else {
+    socketService.sendMessage(currentId.value, message)
+  }
+}
 </script>
