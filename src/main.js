93f5a5e02532bc1e11390fcbb0caaf4406fa4f63
midnight <midnight3424@gmail.com>
2024-08-09 11:03:25 -0500

    refactor: Update Vuetify version to 3.6.14 and remove unused styles import

--- src/main.js
+++ src/main.js
@@ -1,27 +1,22 @@
-// main.js
-// Vuetify
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-// Styles
-import '@/assets/main.css'
-import '../node_modules/flowbite-vue/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-//Components
 import App from './App.vue'
 import router from './router'
-//Store
 import { useAuthStore } from '@/stores/auth'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+// Styles
+import '@/assets/main.css'
+import '../node_modules/flowbite-vue/dist/index.css'
+
+const vuetify = createVuetify()
 
 const app = createApp(App)
 const pinia = createPinia()
+
 pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
