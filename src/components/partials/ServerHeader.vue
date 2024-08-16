d3ec71be68e70e9da08cfd9887a41556ca63795b
midnight <midnight3424@gmail.com>
2024-08-16 11:31:57 -0500

    refactor: Update ServerHeader component to include close chat functionality

--- src/components/partials/ServerHeader.vue
+++ src/components/partials/ServerHeader.vue
@@ -1,76 +1,46 @@
 //src/components/partials/ServerHeader.vue
 <template>
   <div class="flex items-center gap-2">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="h-6 w-6"
+    <button
+      class="ml-auto p-2 rounded-full hover:bg-slate-200 hover:text-red-500"
+      @click="handleCloseChat"
     >
-      <rect width="20" height="8" x="2" y="2" rx="2" ry="2"></rect>
-      <rect width="20" height="8" x="2" y="14" rx="2" ry="2"></rect>
-      <line x1="6" x2="6.01" y1="6" y2="6"></line>
-      <line x1="6" x2="6.01" y1="18" y2="18"></line>
-    </svg>
+    <X/>
+    </button>
+    <Server/>
     <h2 class="text-xl font-bold">{{ serverName }}</h2>
   </div>
   <div class="flex items-center gap-2">
     <button
       class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="h-5 w-5"
-      >
-        <circle cx="11" cy="11" r="8"></circle>
-        <path d="m21 21-4.3-4.3"></path>
-      </svg>
+    <Search />
     </button>
     <button
       class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="h-5 w-5"
-      >
-        <path
-          d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1-1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"
-        ></path>
-        <circle cx="12" cy="12" r="3"></circle>
-      </svg>
+    <Settings />
     </button>
   </div>
 </template>
 
 <script setup>
+import { Server, X, Search, Settings } from 'lucide-vue-next'
+import { useCurrentConversationStore } from '@/stores/conversations/currentConversationStore'
+import { useCurrentChannelStore } from '@/stores/channels/currentChannelStore'
+
+const currentConversationStore = useCurrentConversationStore()
+const currentChannelStore = useCurrentChannelStore()
+
+function handleCloseChat() {
+  currentConversationStore.clearCurrentConversation()
+  currentChannelStore.clearCurrentChannel()
+}
+
 defineProps({
   serverName: {
     type: String,
     required: true
   }
 })
-
-
 </script>
