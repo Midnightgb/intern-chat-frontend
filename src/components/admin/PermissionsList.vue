79671665afbc0b0d08cb76d1da33e8d4a5c18a22
Midnightgb <midnight3424@gmail.com>
2024-10-16 16:31:10 -0500

    Refactor: Utilize generic ListTable component for Permissions
    Replaces the custom PermissionsTable component with a reusable ListTable component to streamline code and improve maintainability. This change promotes consistency and reduces redundancy in handling list views within the admin panel.

--- src/components/admin/PermissionsList.vue
+++ src/components/admin/PermissionsList.vue
@@ -8,7 +8,13 @@
           class="absolute inset-0 bg-gray-100 bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 flex items-center justify-center z-10">
           <fwb-spinner size="12" />
         </div>
-        <PermissionsTable :permissions="permissions" />
+        <ListTable 
+          :items="permissions"
+          :headers="['ID', 'Nombre', 'Estado', 'Creado', 'Actualizado']"
+          :fields="['id_permission', 'name', 'status_permission', 'created_at', 'updated_at']"
+          keyField="id_permission"
+          :formatters="formatters"
+        />
       </div>
     </section>
   </article>
@@ -19,8 +25,8 @@ import { ref, onMounted } from 'vue'
 import { getPermissions } from '@services/api'
 
 import HeaderTab from '@components/common/HeaderTab.vue'
-import PermissionsTable from '@admin/permissions/PermissionsTable.vue'
 import { FwbSpinner } from 'flowbite-vue'
+import ListTable from '@admin/common/ListTable.vue'
 
 const isLoading = ref(false)
 const permissions = ref([])
@@ -42,6 +48,9 @@ async function fetchPermissions() {
   }
 }
 
-
-
+const formatters = {
+  created_at: (value) => new Date(value).toLocaleString('es-ES'),
+  updated_at: (value) => new Date(value).toLocaleString('es-ES'),
+  status_permission: (value) => value ? 'Activo' : 'Inactivo'
+}
 </script>
